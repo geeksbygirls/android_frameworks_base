0157bf10aa0f41d41e21ae5fe367c1e836b7206a
George G <kreach3r@users.noreply.github.com>
2015-11-10 14:59:47 +0200

    GlobalActions: Launch Power Menu via Intent
    Use the intent "android.intent.action.POWER_MENU" from any compatible app, to launch the power menu dialog. No more physical power button presses! :)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2404,6 +2404,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         filter = new IntentFilter(Intent.ACTION_USER_SWITCHED);
         context.registerReceiver(mMultiuserReceiver, filter);
 
+        // register for Power menu (GlobalActions) broadcasts
+        filter = new IntentFilter(Intent.ACTION_POWER_MENU);
+        context.registerReceiver(mGlobalActionsReceiver, filter);
+
         // monitor for system gestures
         mSystemGestures = new SystemGesturesPointerEventListener(context,
                 new SystemGesturesPointerEventListener.Callbacks() {
@@ -7817,6 +7821,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mLastSystemUiFlags = 0;
                     updateSystemUiVisibilityLw();
                 }
+            }
+        }
+    };
+
+    BroadcastReceiver mGlobalActionsReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_POWER_MENU.equals(intent.getAction())) {
+                powerLongPress();
             }
         }
     };
