7acfbd48d633c43d6bb44925c76a99ebc460e831
Thecrazyskull <anaskarbila@aospa.co>
2016-12-29 17:45:46 -0600

    ActivateableNotificationView: re-inflate on configuration change
    Change-Id: Id045b3536805b2fe71b8dddea156440596c8e450
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1662,6 +1662,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
                     initSignalCluster(mHeader);
                     mHeader.setActivityStarter(PhoneStatusBar.this);
+                    mStackScroller.updateNotificationView();
                     mVolumeComponent.updateDialog();
                 }
             });
