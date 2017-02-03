bd38a29380613eddee87b689ac449a9aa5206ff2
ezio84 <brabus84@gmail.com>
2017-02-03 10:15:04 -0700

    Add fade animation to power menu
    Change-Id: Ib0497be22ea7e26dd5628fade680bcedf03d3eca
    
    Conflicts:
    	core/res/res/values/rr_styles.xml

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -357,10 +357,14 @@ public class GlobalActions implements DialogInterface.OnDismissListener, DialogI
                 attrs.gravity = Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL;
         }
         if (powermenuAnimations == 9) {
+                attrs.windowAnimations = R.style.PowerMenuFadeAnimation;
+                attrs.gravity = Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL;
+        }
+        if (powermenuAnimations == 10) {
                 attrs.windowAnimations = R.style.PowerMenuTranslucentAnimation;
                 attrs.gravity = Gravity.TOP|Gravity.CENTER_HORIZONTAL;
         }
-        if (powermenuAnimations == 10) {
+        if (powermenuAnimations == 11) {
                 attrs.windowAnimations = R.style.PowerMenuTranslucentAnimation;
                 attrs.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
         }            
